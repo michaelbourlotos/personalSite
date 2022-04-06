fc0a8eafcfb693d7a770ec214a6b0a18c4374cd7
michaelbourlotos <michaelbourlotos@gmail.com>
2022-04-06 16:08:24 -0400

    added font variable, some image content

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,6 +1,6 @@
 const toggleSwitch = document.querySelector('.dark-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
-
+const testTheme = localStorage.getItem('test')
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
@@ -13,6 +13,7 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        localStorage.setItem('test', 'wazzzzup');
     }
     else {        document.documentElement.setAttribute('data-theme', 'light');
           localStorage.setItem('theme', 'light');
